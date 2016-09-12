72d864e96e851505ea8a6b08661a1b83d2784070
Katy McCann <katy.alice.mccann@gmail.com>
2016-09-12 16:31:32 +0100

    Methods added for print header, print and print footer

--- directory.rb
+++ directory.rb
@@ -13,16 +13,26 @@ students = [
 "Joffrey Baratheon",
 "Norman Bates"
 ]
-# and then print them
-puts "The students of Villains Academy"
-puts "--------------"
 
-students.each do |student|
-	puts student
+def print_header
+	puts "The students of Villains Academy"
+	puts "--------------"
 end
-# finally, we print the total
-print "Overall, we have #{students.count} great students\n"
-#it's important that print() doesn't add new line characters
+
+def print(names)
+	names.each do |name|
+		puts name
+	end
+end
+
+def print_footer(names)
+	puts "Overall, we have #{names.count} great students\n"
+end
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
+
 
 
 
