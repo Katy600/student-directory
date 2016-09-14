cb6a08657e0c7754e15b548360bacc3039f46d31
Katy McCann <katy.alice.mccann@gmail.com>
2016-09-14 10:40:19 +0100

    Add the center method to align the text

--- directory.rb
+++ directory.rb
@@ -1,44 +1,45 @@
 def input_students
+	puts
  
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	puts "Please enter the names of the students".center(100)
+	puts "To finish, just hit return twice".center(100)
 	#create an empty array
 	students = []
 	#get the first name
 	loop do
-	puts "What is the next students name?"
+	puts "What is the next students name?".center(100)
 	name = gets.chomp
 	if name.empty?
 		break
 	end
 
-	puts "What is their hobby?"
+	puts "What is their hobby?".center(100)
 	hobby = gets.chomp
 
-	puts "What is their country of origin?"
+	puts "What is their country of origin?".center(100)
 	country = gets.chomp
 
 	
 		students << {name: name, cohort: :november, hobbies: hobby, country_of_birth: country}
-		puts "Now we have #{students.count} students"
+		puts "Now we have #{students.count} students".center(100)
 	end
 	
 	return students
 end
 
 def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "--------------"
+	puts "The students of my cohort at Makers Academy".center(100)
+	puts "--------------".center(100)
 end
 
 def print(students)
 		students.each_with_index do |student, index|
-		puts "#{index + 1}. #{student[:name]} #{student[:cohort]} cohort. They enjoy #{student[:hobbies]} in their spare time and they come from #{student[:country_of_birth]}"
+		puts "#{index + 1}. #{student[:name]} #{student[:cohort]} cohort. They enjoy #{student[:hobbies]} in their spare time and they come from #{student[:country_of_birth]}".center(100)
 	end
 end
 
 def print_footer(students)
-	puts "Overall, we have #{students.count} great students"
+	puts "Overall, we have #{students.count} great students".center(100)
 end
 #nothing happens until we call the methods
 students = input_students
