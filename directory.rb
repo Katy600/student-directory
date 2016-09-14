92b93dee76013da481a00669e6a784b0b4b0aa3c
Katy McCann <katy.alice.mccann@gmail.com>
2016-09-14 23:47:32 +0100

    Add default values, create month array and until loop for typo, convert string to symbol

--- directory.rb
+++ directory.rb
@@ -1,48 +1,80 @@
+
+MONTHS = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 def input_students
 	puts
  
-	puts "Please enter the names of the students".center(100)
-	puts "To finish, just hit return twice".center(100)
-	#create an empty array
-	students = []
-	#get the first name
-	loop do
-	puts "What is the next students name?".center(100)
-	name = gets.chomp
-	if name.empty?
-		break
-	end
+	puts "Please enter the names of the students".center(50)
+	puts "To finish, just hit return twice".center(50)
 
-	puts "What is their hobby?".center(100)
-	hobby = gets.chomp
+students = []
 
-	puts "What is their country of origin?".center(100)
-	country = gets.chomp
 
-	
-		students << {name: name, cohort: :november, hobbies: hobby, country_of_birth: country}
-		puts "Now we have #{students.count} students".center(100)
-	end
-	
-	return students
+	loop do 
+
+		puts "Enter the students name.".center(50)
+
+		name = gets.chomp
+
+		break if name.empty?
+
+		puts "What cohort are they on?".center(50)
+
+		month = gets.chomp
+			if month == ""
+			   month = "(no month has been entered)"
+				else
+				
+				until MONTHS.any?{|valid_month| month == valid_month} 
+
+				puts "Incorrect spell. Please try again".center(50)
+
+				month = gets.chomp
+				end
+
+			end
+
+		puts "What is their hobby?".center(50)
+
+		hobby = gets.chomp
+			if hobby == ""
+			   hobby = "(no hobbies entered)"
+			end
+
+		puts "What country are they from?".center(50)
+
+		country = gets.chomp
+			if country == ""
+				country = "(no country entered)"
+				
+			end
+
+
+		students << {name: name.to_sym, cohort: month.to_sym, hobby: hobby.to_sym, country: country.to_sym}
+
+		end 
+
+students
 end
 
+	
 def print_header
-	puts "The students of my cohort at Makers Academy".center(100)
-	puts "--------------".center(100)
+	puts "The students of my cohort at Makers Academy".center(50)
+	puts "--------------".center(50)
 end
 
 def print(students)
 		students.each_with_index do |student, index|
-		puts "#{index + 1}. #{student[:name]} #{student[:cohort]} cohort. They enjoy #{student[:hobbies]} in their spare time and they come from #{student[:country_of_birth]}".center(100)
+		puts "#{index + 1}. #{student[:name]} is on the #{student[:cohort]} cohort. They enjoy #{student[:hobby]} in their spare time and they come from #{student[:country]}.".center(50)
 	end
 end
 
 def print_footer(students)
-	puts "Overall, we have #{students.count} great students".center(100)
+	puts "Overall, we have #{students.count} great students".center(50)
 end
 #nothing happens until we call the methods
+
 students = input_students
 print_header
 print(students)
 print_footer(students)
+
