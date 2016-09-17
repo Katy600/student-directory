32162060461743929f7aab1c8a6649331f8dedae
Katy McCann <katy.alice.mccann@gmail.com>
2016-09-17 19:57:10 +0100

    create a seperate method for adding students

--- directory.rb
+++ directory.rb
@@ -1,22 +1,54 @@
 
 @students = []
 
+def print_menue
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "4. Load the list to students.csv"
+    puts "9. Exit"
+  end
+
+def interactive_menu
+    loop do 
+      print_menue
+      process(STDIN.gets.chomp)
+   end
+end
+
+def process(selection)
+case selection
+      when "1"
+      input_students
+      when "2"
+        show_students
+      when "3"
+        save_students
+      when "4"
+        load_students
+      when "9"
+        exit #this will cause the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+   end
+ end 
+
 def input_students
   months = ["January", "February", "March", "April", "May", "June", 
             "July", "August", "September", "October", "November", "December"]
   
-  print "Please enter the name of the student: "
+  puts "Please enter the name of the student: "
   puts "To finish, just hit return twice"
 
-  loop do  
+loop do  
   puts "Add another student" 
-    name = gets[0...-1]
+    name = STDIN.gets[0...-1]
       if name.empty?
         break
       end
 
       print "What cohort should I add #{name} to?: "
-      cohort = gets.chomp
+      cohort = STDIN.gets.chomp
 
       if cohort == ""
          cohort = "november"
@@ -25,16 +57,16 @@ def input_students
       if cohort != ""
         until months.any? {|month| cohort == month} 
           puts "please enter a valid month"
-          cohort = gets.chomp
+          cohort = STDIN.gets.chomp
         end
       end
 
       print "What are #{name}'s hobbies?: "
-      hobbies = gets.chomp
+      hobbies = STDIN.gets.chomp
       print "What is #{name}'s country of birth? "
-      country = gets.chomp
+      country = STDIN.gets.chomp
 
-      @students << {name: name, cohort: cohort.to_sym, hobbies: hobbies, country: country}
+      add_students
       
       if @students.count == 1
         puts "Now we have #{@students.count} student".center(100)
@@ -46,43 +78,11 @@ def input_students
   end
 end
 
-def interactive_menu
-    loop do 
-      print_menue
-      process(gets.chomp)
-   end
-end
-
-def print_menue
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "3. Save the list to students.csv"
-    puts "4. Load the list to students.csv"
-    puts "9. Exit"
-  end
-
 def show_students
       output_header
       output
       output_footer
   end
-
-def process(selection)
-case selection
-      when "1"
-        #input the students
-      input_students
-      when "2"
-        #show the students
-        show_students
-      when "3"
-        save_students
-      when "9"
-        exit #this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-   end
- end 
 
 def output_header
   if @students.count == 0
@@ -151,14 +151,30 @@ def save_students
   file.close
 end
 
-def load_students
+def load_students(filename = "students.csv")
   file = File.open("students.csv", "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+   add_students
   end 
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
 
+def add_students
+  @students << {name: name, cohort: cohort.to_sym}  
+end
+
+try_load_students
 interactive_menu
